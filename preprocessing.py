860d9bfc50ea6b640cb57fb3815ff03448efd327
Beyzanur Kuyuk <bka81@sfu.ca>
2025-06-30 13:53:27 -0700

    Check the processed data

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("oasis_cross-sectional.csv")
 
-print(df.head())
+
 df = df.drop(columns=["ID", "Hand", "Delay"])
 
 df = df[df["CDR"].notna()]
@@ -16,3 +16,4 @@ df["M/F"] = df["M/F"].map({"M":1, "F": 0})
 features_to_scale = ["Age", "Educ", "SES", "MMSE", "eTIV", "nWBV", "ASF"]
 scaler = StandardScaler()
 df[features_to_scale] = scaler.fit_transform(df[features_to_scale])
+print(df)
