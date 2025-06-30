ca39795fdfcc3685665aaec346a39976c4d75f98
Beyzanur Kuyuk <bka81@sfu.ca>
2025-06-30 13:21:15 -0700

    chore: preprocess the dataset

---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("oasis_cross-sectional.csv")
+
+print(df.head())
+df = df.drop(columns=["ID", "Hand", "Delay"])
+
+df = df[df["CDR"].notna()]
+df["Dementia"] = df["CDR"].apply(lambda x: 1 if x> 0 else 0)
+for col in ["MMSE", "SES", "Educ"]:
+    df[col] = df[col].fillna(df[col].median())
+
+df["M/F"] = df["M/F"].map({"M":1, "F": 0})
+
+features_to_scale = ["Age", "Educ", "SES", "MMSE", "eTIV", "nWBV", "ASF"]
+scaler = StandardScaler()
+df[features_to_scale] = scaler.fit_transform(df[features_to_scale])
